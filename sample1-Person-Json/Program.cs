e0c5e67167ddcad0e9fcb11015639cd88b114ba4
Luc Vo Van <lvovan@microsoft.com>
2021-01-12 00:19:06 +0100

    Local projects done

---
+++ sample1-Person-Json/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace sample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myPerson = new Person()
+            {
+                Name = "Luc",
+                Age = 41
+            };
+
+            // var jsonStr = JsonConvert.SerializeObject(myPerson);
+            var jsonStr = JsonConvert.SerializeObject(myPerson, new JsonSerializerSettings() { Formatting = Formatting.Indented });
+
+            Console.WriteLine(jsonStr);
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string Hello(bool isLowercase)
+        {
+            var res = $"HELLO {Name}, YOU ARE {Age}";
+            if(isLowercase)
+                return res.ToLower();
+            return res;
+        }
+    }
+}
